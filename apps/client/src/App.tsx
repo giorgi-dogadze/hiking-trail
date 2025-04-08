ab782a221e99cd32cad51e9daf9079de44aad3c3
Giorgi Dogadze <giorgi@primaryportal.com>
2025-04-08 10:30:53 +0400

    fix: update link text in App component from 'Learn React' to 'hiking trial'

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hiking trial
         </a>
       </header>
     </div>
