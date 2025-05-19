648bac55866b51444940a686a1479ca87e62e570
Giorgi Dogadze <giorgi@primaryportal.com>
2025-05-19 16:52:59 +0400

    feat: set base URL for routing to /hiking-trail

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -8,7 +8,7 @@ import { LanguageProvider } from './context/LanguageContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hiking-trail">
       <LanguageProvider>
         <App />
       </LanguageProvider>
